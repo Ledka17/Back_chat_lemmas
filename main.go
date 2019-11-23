6b42e2dd5ad36edaaff5bbe7f03aae614f7574aa
Ledka17 <ledkakonf@gmail.com>
2019-11-23 12:35:55 +0300

    add http

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"log"
 	"net/http"
 	"os"
+	user "github.com/Ledka17/Back_chat_lemmas/delivery/ws"
+	"github.com/labstack/echo"
 )
 
 func main() {
+	e = echo.New()
+
+	init
+	http.Handle("/message", user.GetMessagesHandler)
+
 	log.Println("http server started on :8000")
 	port := getPort()
 	log.Fatal(http.ListenAndServe(port, nil))
